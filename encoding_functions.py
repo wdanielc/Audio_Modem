2e01ea01e8f3fa0f06fd58994bbc478a7ce67f18
W.D. Connolley <wdanielc@googlemail.com>
2019-05-13 15:25:16 +0100

    Started on QAM encoding functions

--- encoding_functions.py
+++ encoding_functions.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class Constellation():
     pass
@@ -9,4 +9,15 @@ def bits2values():
 
 
 def OFDM():
+    pass
+
+
+def QAM(in, num): #1 = 4QAM, 2=16QAM, ...
+    a = in % (1 << num)
+    b = in >> (num)
+    a = 1 - 2**num + 2*grey(a,num)
+    b = 1 - 2**num + 2*grey(b,num)
+    return (a + b*i)    #close butt no cigar
+
+def grey(in,num): #todo
     pass
