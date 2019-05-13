87cecc21f7487d7d82b85ab69acf7f8fd6bedb85
W.D. Connolley <wdanielc@googlemail.com>
2019-05-13 16:30:07 +0100

    Normalised energy of QAM constellations

--- Transmit.py
+++ Transmit.py
@@ -5,3 +5,11 @@ Created on Mon May 13 14:15:46 2019
 
 @author: wdc24
 """
+
+a = 'Test'
+b = bytearray(a,'ascii')
+print(b[3])
+print(bin(b[0]))
+print(bin(b[1]))
+print(bin(b[2]))
+print(bin(b[3]))
--- encoding_functions.py
+++ encoding_functions.py
@@ -8,20 +8,21 @@ def bits2values():
     pass
 
 
-def OFDM():
+def OFDM(symbol,):
+    
     pass
 
+QAM_norm = [2,10,42]
 
 def QAM(var, num): #1 = 4QAM, 2=16QAM, ...
     a = var % (1 << num)
     b = var >> (num)
     a = 1 - 2**num + 2*bin2grey(a)
     b = 1 - 2**num + 2*bin2grey(b)
-    return complex(a,b)
+    return (complex(a,b)/(QAM_norm[num-1]**0.5))
 
 def bin2grey(var):
     return(var ^ (var >> 1))
-
 
 def grey2bin(var):
     mask = var >> 1
