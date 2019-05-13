285e12ad04766d6d290ae29282d6daba7723c349
W.D. Connolley <wdanielc@googlemail.com>
2019-05-13 17:22:01 +0100

    added fn to upconvert OFDM into real transmittable signal

--- Transmit.py
+++ Transmit.py
@@ -5,11 +5,19 @@ Created on Mon May 13 14:15:46 2019
 
 @author: wdc24
 """
+import encoding_functions as encode
 
-a = 'Test'
-b = bytearray(a,'ascii')
-print(b[3])
-print(bin(b[0]))
-print(bin(b[1]))
-print(bin(b[2]))
-print(bin(b[3]))
+Fc = 10000
+dF = 100
+T = 1/dF 
+
+data = b'test encode'
+len(data)
+symbol = []
+for i in range(len(data)):
+    symbol.append(encode.QAM(data[i] >> 4,2))
+    symbol.append(encode.QAM(data[i] % 16,2))
+
+transmit = encode.OFDM(symbol,2)
+Ts = T/len(transmit)
+transmit = encode.upconvert(transmit, Fc, Ts)
--- encoding_functions.py
+++ encoding_functions.py
@@ -8,9 +8,10 @@ def bits2values():
     pass
 
 
-def OFDM(symbol,):
-    
-    pass
+def OFDM(symbol,Lp):
+    trans = np.fft.ifft(symbol)
+    trans = np.insert(trans, 0, trans[-Lp:])
+    return trans
 
 QAM_norm = [2,10,42]
 
@@ -30,3 +31,10 @@ def grey2bin(var):
         var = var ^ mask
         mask = mask >> 1
     return var
+
+def upconvert(symbol,Fc,Ts):
+    transmit = []
+    freq = 2 * np.pi * Fc * Ts
+    for i in range(len(symbol)):
+        transmit.append(symbol[i].real * np.cos(i * freq) + symbol[i].imag * np.sin(i * freq))
+    return transmit
